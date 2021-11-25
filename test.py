b20de69958fb2c2ba94e4d54460f476469d1f348
kastastin <kostya242421@gmai.com>
2021-11-25 05:40:48 +0200

    initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def testSearch():
+    searchWorld = 'book'
+
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(15)
+
+    driver.get('https://www.moyo.ua/')
+    driver.find_elements_by_css_selector('#search-input')[0].send_keys(searchWorld)
+    driver.find_elements_by_css_selector('#search-input')[0].send_keys(Keys.ENTER)
+
+    actualText = driver.find_elements_by_css_selector('.search_title span')[0].text
+    expectedText = 'book'
+
+    assert actualText == expectedText, 'Red test'
+    # python -m pytest to test
